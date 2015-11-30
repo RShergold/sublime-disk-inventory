078b2b136269d35355356f2adeccc232afe7d769
Remi <remi.shergold@hotmail.com>
2015-11-30 14:14:19 +0000

    removed global variable

--- diskinventoryx.py
+++ diskinventoryx.py
@@ -1,22 +1,18 @@
 import sublime, sublime_plugin, os
 from subprocess import call
 
-global diskInventoryXPath
-diskInventoryXPath = "/Applications/Disk Inventory X.app"
-
-
 def plugin_loaded():
   if diskInventoryIsInstalled() is False:
-    sublime.error_message("Disk Inventory X is not installed at \n" + DiskInventoryXPath)
+    sublime.error_message("Disk Inventory X is not installed at \n /Applications/Disk Inventory X.app")
 
 def diskInventoryIsInstalled():
-  return os.path.exists(DiskInventoryXPath)
+  return os.path.exists("/Applications/Disk Inventory X.app")
 
 class DiskInventoryCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
     for path in paths:
       if os.path.isdir(path):
-        call(["open", DiskInventoryXPath, "--args", path])
+        call(["open", "/Applications/Disk Inventory X.app", "--args", path])
 
   def is_visible(self, paths =[]):
     if diskInventoryIsInstalled():
